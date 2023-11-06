5803a2c23b3c1907ddc3239dcabf8b3e418721f6
solahidris <emailsolah@gmail.com>
2023-11-06 21:34:18 +0800

    test

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+// import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-[100vh] p-4">
+      <div>
+        <p>hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+        <p className="bg-red-600 p-4">hi</p>
+      </div>
+    </main>
+  )
+}
