4181b94a034c4f8f696a05746bfcee7c3cf25c96
solahidris <emailsolah@gmail.com>
2023-11-06 22:35:48 +0800

    3D

---
+++ components/ThreeDObject.tsx
@@ -0,0 +1,83 @@
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { useRef, useLayoutEffect } from "react";
+import { useTransform, useScroll, useTime } from "framer-motion";
+import { degreesToRadians, progress, mix } from "popmotion";
+import * as THREE from 'three';
+
+
+const color = "#111111";
+
+const Icosahedron = () => (
+  <mesh rotation-x={0.35}>
+    <icosahedronGeometry args={[1, 0]} />
+    <meshBasicMaterial wireframe color={color} />
+  </mesh>
+);
+
+const Star = ({ p }: { p: number }) => {
+  const ref = useRef<THREE.Mesh>(null); // Use RefObject of type Mesh
+
+  useLayoutEffect(() => {
+    const distance = mix(2, 3.5, Math.random());
+    const yAngle = mix(
+      degreesToRadians(80),
+      degreesToRadians(100),
+      Math.random()
+    );
+    const xAngle = degreesToRadians(360) * p;
+    ref.current!.position.setFromSphericalCoords(distance, yAngle, xAngle);
+  });
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={[0.05, 0.05, 0.05]} />
+      <meshBasicMaterial wireframe color={color} />
+    </mesh>
+  );
+};
+
+function Scene({ numStars = 100 }) {
+  const gl = useThree((state) => state.gl);
+  const { scrollYProgress } = useScroll();
+  const yAngle = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0.001, degreesToRadians(180)]
+  );
+  const distance = useTransform(scrollYProgress, [0, 1], [10, 3]);
+  const time = useTime();
+
+  useFrame(({ camera }) => {
+    camera.position.setFromSphericalCoords(
+      distance.get(),
+      yAngle.get(),
+      time.get() * 0.0005
+    );
+    camera.updateProjectionMatrix();
+    camera.lookAt(0, 0, 0);
+  });
+
+  useLayoutEffect(() => gl.setPixelRatio(0.3));
+
+  const stars = [];
+  for (let i = 0; i < numStars; i++) {
+    stars.push(<Star p={progress(0, numStars, i)} />);
+  }
+
+  return (
+    <>
+      <Icosahedron />
+      {stars}
+    </>
+  );
+}
+
+export default function ThreeDObject() {
+  return (
+    <div className="container">
+      <Canvas gl={{ antialias: false }}>
+        <Scene />
+      </Canvas>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
 import { useState } from "react";
 // import { motion } from "framer-motion";
+import ThreeDObject from "@/components/ThreeDObject";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-[100vh] p-4">
-      <div>
-        <p>hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-        <p className="bg-red-600 p-4">hi</p>
-      </div>
-    </main>
+    <div>
+      <ThreeDObject />
+    </div>
   )
 }
