d866fb9a481ed33a3804e1988e225a95889c3aa3
solahidris <emailsolah@gmail.com>
2023-11-09 19:15:57 +0800

    Complete basic

--- components/ParallexPage.tsx
+++ components/ParallexPage.tsx
@@ -6,7 +6,7 @@ import { motion, useScroll, useSpring, useTransform, MotionValue } from "framer-
 import BlobAnimation from "./BlobAnimation";
 import RightMenu from "./RightMenu";
 import { MapPinIcon, InboxIcon, PhoneIcon, ChevronDoubleUpIcon, GlobeAsiaAustraliaIcon } from "@heroicons/react/20/solid";
-
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 const BlobImageParallax = ({ id }: { id: number }) => {
   const ref = useRef(null);
@@ -101,6 +101,10 @@ const ParallexPage = () => {
               <span className="font-mono text-xs">based in</span>
               <span className="font-mono text-xs">Kuala Lumpur,</span>
               <span className="font-mono text-xs flex gap-1">Malaysia <GlobeAsiaAustraliaIcon className="w-[12px]" /></span>
+            </div>
+            <div className="flex pt-8 gap-x-4 justify-start lg:justify-start">
+              <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://github.com/solahidris"><SiGithub className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] animate-pulse hover:text-yellow-400"/></a></motion.button>
+              <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/solahuddinidris/"><SiLinkedin className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] animate-pulse hover:text-blue-500"/></a></motion.button>
             </div>
           </div>
         </div>
@@ -247,11 +251,7 @@ const ParallexPage = () => {
               </div>
             </div>
 
-            <div className="fixed bottom-0 left-0 bg-red-4000 p-5">
-              <motion.button whileTap={{ scale: 0.80 }} whileHover={{ scale: 1.1 }} onClick={() => {homeRef.current!.scrollIntoView({ behavior: 'smooth' })}} className="bg-neutral-900 w-[50px] h-[50px] rounded flex items-center justify-center">
-                <span><ChevronDoubleUpIcon className="w-[40px] h-[40px] text-white/20"/></span>
-              </motion.button>
-            </div>
+ 
         </div>
       ),
     },
@@ -270,6 +270,20 @@ const ParallexPage = () => {
         </div>
       ))}
       <motion.div className="progress" style={{ scaleX }} />
+
+      {/* Footer - Github and Linkedin */}
+      <div className="fixed bottom-0 flex left-0 mb-6 ml-[100px] gap-x-4 justify-start lg:justify-start">
+        <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://github.com/solahidris"><SiGithub className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] hover:text-yellow-300 text-neutral-100/50"/></a></motion.button>
+        <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/solahuddinidris/"><SiLinkedin className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] hover:text-cyan-300 text-neutral-100/50"/></a></motion.button>
+      </div>
+      
+      {/* Footer - Scroll to Top Function */}
+      <div className="fixed bottom-0 left-0 bg-red-4000 p-5">
+        <motion.button whileTap={{ scale: 0.80 }} whileHover={{ scale: 1.1 }} onClick={() => {homeRef.current!.scrollIntoView({ behavior: 'smooth' })}} className="bg-neutral-900 w-[50px] h-[50px] rounded flex items-center justify-center">
+          <span><ChevronDoubleUpIcon className="w-[40px] h-[40px] text-white/20"/></span>
+        </motion.button>
+      </div>
+      
     </div>
   );
 }
