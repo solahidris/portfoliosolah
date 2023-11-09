ef0dbeda9df4a98e1037cf142068c309a0de6aca
solahidris <emailsolah@gmail.com>
2023-11-09 22:28:07 +0800

    remove footer and lowered the x progress and set y mandatory

--- components/ParallexPage.tsx
+++ components/ParallexPage.tsx
@@ -259,9 +259,11 @@ const ParallexPage = () => {
 
   return (
     <div className="flex flex-col z-10 px-2 relative min-h-screen"> 
+
       <div className="absolute top-0 left-0 w-full z-40">
         <RightMenu scrollToSection={scrollToSection} />
       </div>
+
       {imageData.map((data) => (
         <div key={data.id} className="flex">
           {data.content}
@@ -272,17 +274,17 @@ const ParallexPage = () => {
       <motion.div className="progress" style={{ scaleX }} />
 
       {/* Footer - Github and Linkedin */}
-      <div className="fixed bottom-0 flex left-0 mb-6 ml-[100px] gap-x-4 justify-start lg:justify-start">
+      {/* <div className="fixed bottom-0 flex left-0 mb-6 ml-[100px] gap-x-4 justify-start lg:justify-start">
         <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://github.com/solahidris"><SiGithub className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] hover:text-yellow-300 text-neutral-100/50"/></a></motion.button>
         <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/solahuddinidris/"><SiLinkedin className="w-[1.7rem] h-[1.7rem] lg:w-[2rem] lg:h-[2rem] hover:text-cyan-300 text-neutral-100/50"/></a></motion.button>
-      </div>
+      </div> */}
       
       {/* Footer - Scroll to Top Function */}
-      <div className="fixed bottom-0 left-0 bg-red-4000 p-5 z-[200px]">
+      {/* <div className="fixed bottom-0 left-0 bg-red-4000 p-5 z-[200px]">
         <motion.button whileTap={{ scale: 0.80 }} whileHover={{ scale: 1.1 }} onClick={() => {homeRef.current!.scrollIntoView({ behavior: 'smooth' })}} className="bg-neutral-900 w-[50px] h-[50px] rounded flex items-center justify-center">
           <span><ChevronDoubleUpIcon className="w-[40px] h-[40px] text-white/20"/></span>
         </motion.button>
-      </div>
+      </div> */}
       
     </div>
   );
