96f2e075b315b21c0f6b68adec9fba97e383ab2a
solahidris <emailsolah@gmail.com>
2023-11-12 00:02:34 +0800

    Add number loading

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -1,7 +1,6 @@
 // component/LoadingPage.tsx
 
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-// import { motion } from "framer-motion";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { useEffect } from "react";
 
@@ -14,20 +13,33 @@ const LoadingPage = () => {
     return animation.stop; // Stop
   }, []);
 
-  return(
-    <div className="flex flex-col gap-0 justify-center items-center min-h-screen">
+  const barWidth = useTransform(count, [0, 96], ["0%", "96%"]); // Translate count to width
 
+
+  return(
+    <div className="flex flex-col gap-0 justify-center items-center min-h-screen pb-[120px]">
+      
       <motion.div initial={{ opacity:0, scale:0.75 }} animate={{ opacity:1, scale:1 }} transition={{ duration: 0.7, delay: 0.5 }} 
-      className="flex flex-col justify-center items-center gap-4">
+      className="flex flex-col justify-center items-center gap-4 pb-6">
         <AiOutlineLoading3Quarters className="w-5 h-5 lg:w-6 lg:h-6 animate-spin w-full"/>
         <span className="font-mono tracking-widest animate-pulse">Loading...</span>
       </motion.div>
 
-      <div className="pt-10">
-        <motion.span className="font-mono text-[30px]">{rounded}</motion.span>
+      {/* Load bar animation */}
+      <div className="mt-[18px] w-64 h-[30px] bg-neutral-200/90 rounded-md">
+        <motion.div
+          className="w-64 h-[22px] ml-[4px] mt-[4px] bg-gradient-to-r from-blue-700 via-blue-600 to-blue-700 rounded-md"
+          style={{ width: barWidth }}
+        />
       </div>
 
+      {/* Number count 0-100 */}
+      <div className="flex gap-2 items-end justify-center">
+        <motion.span className="font-mono text-[24px] mt-4 tracking-wide text-white/80">{rounded}</motion.span>
+        <span className="mb-[5px] text-white/80">%</span>
       </div>
+
+    </div>
   )
 };
 
