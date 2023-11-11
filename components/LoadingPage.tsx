1011a384e21739a42927c3d3700f71c1701d2b37
solahidris <emailsolah@gmail.com>
2023-11-11 14:30:24 +0800

    Added minimal UI on Loading page

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const LoadingPage = () => {
   return(
     <div className="flex justify-center items-center min-h-screen">
-      <p className="font-mono tracking-widest">Loading...</p>
+      <div className="flex flex-col justify-center gap-4">
+        <AiOutlineLoading3Quarters className="w-5 h-5 lg:w-6 lg:h-6 animate-spin w-full"/>
+        <span className="font-mono tracking-widest animate-pulse">Loading...</span>
+      </div>
     </div>
   )
 };
