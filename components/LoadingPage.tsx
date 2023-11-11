c04b355cb3db2f0993f9a8c0fd86ede586f00e9d
solahidris <emailsolah@gmail.com>
2023-11-11 14:25:46 +0800

    Added a Loading Page before homepage

---
+++ components/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const LoadingPage = () => {
+  return(
+    <div className="flex justify-center items-center min-h-screen">
+      <p className="font-mono tracking-widest">Loading...</p>
+    </div>
+  )
+};
+
+export default LoadingPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,35 @@
 
 import ThreeDObject from "@/components/ThreeDObject";
 import ParallexPage from "@/components/ParallexPage";
+import React, { useState, useEffect } from "react";
+import LoadingPage from "@/components/LoadingPage";
 
-export default function Home() {
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(true); // Loading State
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false); // Set loading to false after
+    }, 500); // Time duration - 0.5s
+
+    // Clear the timeout on component unmount
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <ThreeDObject />
-      <ParallexPage />
+      {isLoading ? (
+        <>
+          <LoadingPage />
+        </>
+      ) : (
+        <>
+          <ThreeDObject />
+          <ParallexPage />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default Home;
