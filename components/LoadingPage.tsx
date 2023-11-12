c2c37fda8a91387b34fd95c44fc3f12750e42587
solahidris <emailsolah@gmail.com>
2023-11-12 12:17:10 +0800

    Yellow Loading Bar

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -41,18 +41,12 @@ const LoadingPage: React.FC<LoadingPageProps> = ({isLoading}) => {
       className="flex flex-col justify-center items-center gap-0 pb-0 mb-[-50px]">
         <LottieAnimationNyan />
         <LottieAnimationLoading />
-        {/* <div dangerouslySetInnerHTML={{
-          __html: `
-            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-            <lottie-player src="https://lottie.host/d1578c28-8a4e-47d9-b9c3-08e715a10616/bNZvqUVdpO.json" background="transparent" speed="1" style="width: 200px; height: 200px" loop autoplay direction="1" mode="normal"></lottie-player>
-          `
-        }}></div> */}
       </div>
 
       {/* Load bar animation */}
       <div className="mt-[18px] w-64 h-[30px] bg-neutral-600/90 rounded-md">
         <motion.div
-          className="w-64 h-[22px] ml-[4px] mt-[4px] bg-gradient-to-r from-cyan-400 via-cyan-300 to-cyan-400 rounded-md"
+          className="w-64 h-[22px] ml-[4px] mt-[4px] bg-gradient-to-r from-amber-500 via-amber-400 to-amber-600 rounded-md"
           style={{ width: barWidth }}
         />
       </div>
